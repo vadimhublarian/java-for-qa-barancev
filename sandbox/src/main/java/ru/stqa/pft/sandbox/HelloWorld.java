9ace7abe17ec589dbb1e34e47819f3d8de0d3777
Vadim Khublarian <vadimjedi@gmail.com>
2018-10-03 00:48:57 +0300

    Added some method examples

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -4,7 +4,27 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        hello("World");
+        hello("user");
+        hello("Nick");
+
+        double squareSideLen = 5;
+        System.out.println("An area of a square with a side " + squareSideLen + " = " + area(squareSideLen));
+
+        double sideAlen = 4;
+        double sideBlen = 6;
+        System.out.println("An area of a rectangle with a side " + sideAlen + " and " + sideBlen + " = " + area(sideAlen, sideBlen));
     }
 
+    public static void hello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
